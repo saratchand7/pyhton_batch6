3357960c65d1c913da99b591527aad57ce99a508
Sarat chand <sarathchintala57@gmail.com>
2026-09-01 13:59:28 +0530

    Learned about tuple

---
+++ python day 7.py
@@ -0,0 +1,127 @@
+
+
+Tuple
+--------
+
+--> tuple is  collection of different datatypes that separated by , and represented by ()
+
+--> it is immutable
+
+eg:-
+
+t = (1,'python',[3,4],(7,9))
+print(t[2])
+
+
+index()
+---------
+
+-->if item is not present in the tuple, it will rise valueError
+
+
+
+eg:-
+
+t = (1,'python',[3,4],(7,9))
+print(t.index('python'))
+
+len()
+-------
+
+-->
+
+
+eg:-
+
+t = (1,'python',[3,4],(7,9))
+print(len(t))
+
+-----------
+tuple()
+
+--> Tuple is collection of different datatypes that separated by, and represented by ()
+-> it is immutable
+-> we can pass a tuple values and that can be asing to the variables, but should match same number varibles and values inside the tuple
+
+eg:-
+
+name,  age = ('sarat', 34)
+print(name)
+print(age)
+
+============
+
+max()
+---------
+
+--> used to findout the max value from the tuple
+
+eg:-
+
+so = (67,5,89,45)
+print(max(so))
+
+
+min()
+------------
+
+--> min used to find out the leaste value in tuple
+
+eg:-
+
+so = (67,5,89,45)
+print(min(so))
+
+count()
+--------
+
+--> count used to count an item present in the tuple
+
+eg:-
+
+so = (67,5,89,45)
+print(so.count(5))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
